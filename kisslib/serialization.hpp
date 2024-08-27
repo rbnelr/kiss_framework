c95a075b45a25a5de3315967a431d6e8d5e4dd89
Anon Ymous <rbnelr@gmx.de>
2024-08-27 04:53:36 +0200

    small fix

--- kisslib/serialization.hpp
+++ kisslib/serialization.hpp
@@ -87,38 +87,45 @@ inline T deserialize (char const* filename) {
 	return t;
 }
 
-#define _JSON_EXPAND( x ) x
-#define _JSON_GET_MACRO(_1,_2,_3,_4,_5,_6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, NAME,...) NAME
-
-#define _JSON_PASTE(...) _JSON_EXPAND(_JSON_GET_MACRO(__VA_ARGS__, \
-	_JSON_PASTE20, _JSON_PASTE19, _JSON_PASTE18, _JSON_PASTE17, _JSON_PASTE16, _JSON_PASTE15, _JSON_PASTE14, _JSON_PASTE13, _JSON_PASTE12, _JSON_PASTE11, \
-	_JSON_PASTE10, _JSON_PASTE9, _JSON_PASTE8, _JSON_PASTE7, _JSON_PASTE6, _JSON_PASTE5, _JSON_PASTE4, _JSON_PASTE3, _JSON_PASTE2, _JSON_PASTE1 \
-	)(__VA_ARGS__))
-#define _JSON_PASTE2(func,  v1)																						func(v1)
-#define _JSON_PASTE3(func,  v1, v2)																					_JSON_PASTE2(func, v1) _JSON_PASTE2(func, v2)
-#define _JSON_PASTE4(func,  v1, v2, v3)																				_JSON_PASTE2(func, v1) _JSON_PASTE3(func, v2, v3)
-#define _JSON_PASTE5(func,  v1, v2, v3, v4)																			_JSON_PASTE2(func, v1) _JSON_PASTE4(func, v2, v3, v4)
-#define _JSON_PASTE6(func,  v1, v2, v3, v4, v5)																		_JSON_PASTE2(func, v1) _JSON_PASTE5(func, v2, v3, v4, v5)
-#define _JSON_PASTE7(func,  v1, v2, v3, v4, v5, v6)																	_JSON_PASTE2(func, v1) _JSON_PASTE6(func, v2, v3, v4, v5, v6)
-#define _JSON_PASTE8(func,  v1, v2, v3, v4, v5, v6, v7)																_JSON_PASTE2(func, v1) _JSON_PASTE7(func, v2, v3, v4, v5, v6, v7)
-#define _JSON_PASTE9(func,  v1, v2, v3, v4, v5, v6, v7, v8)															_JSON_PASTE2(func, v1) _JSON_PASTE8(func, v2, v3, v4, v5, v6, v7, v8)
-#define _JSON_PASTE10(func, v1, v2, v3, v4, v5, v6, v7, v8, v9)														_JSON_PASTE2(func, v1) _JSON_PASTE9(func, v2, v3, v4, v5, v6, v7, v8, v9)
-#define _JSON_PASTE11(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10)												_JSON_PASTE2(func, v1) _JSON_PASTE10(func, v2, v3, v4, v5, v6, v7, v8, v9, v10)
-#define _JSON_PASTE12(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11)											_JSON_PASTE2(func, v1) _JSON_PASTE11(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11)
-#define _JSON_PASTE13(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12)										_JSON_PASTE2(func, v1) _JSON_PASTE12(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12)
-#define _JSON_PASTE14(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13)									_JSON_PASTE2(func, v1) _JSON_PASTE13(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13)
-#define _JSON_PASTE15(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14)							_JSON_PASTE2(func, v1) _JSON_PASTE14(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14)
-#define _JSON_PASTE16(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15)						_JSON_PASTE2(func, v1) _JSON_PASTE15(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15)
-#define _JSON_PASTE17(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16)					_JSON_PASTE2(func, v1) _JSON_PASTE16(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16)
-#define _JSON_PASTE18(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17)				_JSON_PASTE2(func, v1) _JSON_PASTE17(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17)
-#define _JSON_PASTE19(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18)		_JSON_PASTE2(func, v1) _JSON_PASTE18(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18)
-#define _JSON_PASTE20(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19)	_JSON_PASTE2(func, v1) _JSON_PASTE19(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19)
+// Terrifying macro abomination GO!
 
 #define _JSON_TO(v1) j[#v1] = t.v1;
 #define _JSON_FROM(v1) if (j.contains(#v1)) j.at(#v1).get_to(t.v1); // try get value from json
 
+#define _JSON_EXPAND( x ) x
+#define _JSON_GET_MACRO(_0,_1,_2,_3,_4,_5,_6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, NAME, ...) NAME
+
+#define _JSON_PASTE(...) _JSON_EXPAND(_JSON_GET_MACRO(__VA_ARGS__, \
+	_JSON_PASTE20, _JSON_PASTE19, _JSON_PASTE18, _JSON_PASTE17, _JSON_PASTE16, _JSON_PASTE15, _JSON_PASTE14, _JSON_PASTE13, _JSON_PASTE12, _JSON_PASTE11, \
+	_JSON_PASTE10, _JSON_PASTE9, _JSON_PASTE8, _JSON_PASTE7, _JSON_PASTE6, _JSON_PASTE5, _JSON_PASTE4, _JSON_PASTE3, _JSON_PASTE2, _JSON_PASTE1, _JSON_PASTE0 \
+	)(__VA_ARGS__))
+
+#define _JSON_PASTE0(func)
+#define _JSON_PASTE1(func,  v1)                                                                                         func(v1)
+#define _JSON_PASTE2(func,  v1, v2)                                                                                     func(v1) _JSON_PASTE1(func, v2)
+#define _JSON_PASTE3(func,  v1, v2, v3)                                                                                 func(v1) _JSON_PASTE2(func, v2, v3)
+#define _JSON_PASTE4(func,  v1, v2, v3, v4)                                                                             func(v1) _JSON_PASTE3(func, v2, v3, v4)
+#define _JSON_PASTE5(func,  v1, v2, v3, v4, v5)                                                                         func(v1) _JSON_PASTE4(func, v2, v3, v4, v5)
+#define _JSON_PASTE6(func,  v1, v2, v3, v4, v5, v6)                                                                     func(v1) _JSON_PASTE5(func, v2, v3, v4, v5, v6)
+#define _JSON_PASTE7(func,  v1, v2, v3, v4, v5, v6, v7)                                                                 func(v1) _JSON_PASTE6(func, v2, v3, v4, v5, v6, v7)
+#define _JSON_PASTE8(func,  v1, v2, v3, v4, v5, v6, v7, v8)                                                             func(v1) _JSON_PASTE7(func, v2, v3, v4, v5, v6, v7, v8)
+#define _JSON_PASTE9(func,  v1, v2, v3, v4, v5, v6, v7, v8, v9)                                                         func(v1) _JSON_PASTE8(func, v2, v3, v4, v5, v6, v7, v8, v9)
+#define _JSON_PASTE10(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10)                                                    func(v1) _JSON_PASTE9(func, v2, v3, v4, v5, v6, v7, v8, v9, v10)
+#define _JSON_PASTE11(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11)                                               func(v1) _JSON_PASTE10(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11)
+#define _JSON_PASTE12(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12)                                          func(v1) _JSON_PASTE11(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12)
+#define _JSON_PASTE13(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13)                                     func(v1) _JSON_PASTE12(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13)
+#define _JSON_PASTE14(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14)                                func(v1) _JSON_PASTE13(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14)
+#define _JSON_PASTE15(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15)                           func(v1) _JSON_PASTE14(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15)
+#define _JSON_PASTE16(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16)                      func(v1) _JSON_PASTE15(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16)
+#define _JSON_PASTE17(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17)                 func(v1) _JSON_PASTE16(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17)
+#define _JSON_PASTE18(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18)            func(v1) _JSON_PASTE17(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18)
+#define _JSON_PASTE19(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19)       func(v1) _JSON_PASTE18(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19)
+#define _JSON_PASTE20(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20)  func(v1) _JSON_PASTE19(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20)
+
 #define SERIALIZE_TO_JSON(Type) void to_json(nlohmann::ordered_json& j, const Type& t)
 #define SERIALIZE_FROM_JSON(Type) void from_json(const nlohmann::ordered_json& j, Type& t)
+
+// Cannot handle zero arguments because preprocessor is badly designed
 
 #define SERIALIZE_TO_JSON_EXPAND(...)   _JSON_EXPAND(_JSON_PASTE(_JSON_TO, __VA_ARGS__))
 #define SERIALIZE_FROM_JSON_EXPAND(...) _JSON_EXPAND(_JSON_PASTE(_JSON_FROM, __VA_ARGS__))
@@ -131,15 +138,11 @@ inline T deserialize (char const* filename) {
     SERIALIZE_TO_JSON(Type)   { SERIALIZE_TO_JSON_EXPAND(__VA_ARGS__)   } \
     SERIALIZE_FROM_JSON(Type) { SERIALIZE_FROM_JSON_EXPAND(__VA_ARGS__) }
 
-// Empty for convinence (I have no idea how to add 0 args to the macro mess above -_-)
-#define SERIALIZE_NONE(Type) \
-    friend SERIALIZE_TO_JSON(Type)   {} \
+// Macros can't handle no arguments, instead use this version if class gets serialized somehere but calls does not want to serialize any members
+// assign an ampty object because otherside j ends up as null
+#define SERIALIZE_NONE(Type, ...)  \
+    friend SERIALIZE_TO_JSON(Type)   { j = nlohmann::ordered_json::object(); } \
     friend SERIALIZE_FROM_JSON(Type) {}
-
-#define SERIALIZE_OUT_OF_CLASS_NONE(Type)  \
-    friend SERIALIZE_TO_JSON(Type)   {} \
-    friend SERIALIZE_FROM_JSON(Type) {}
-
 
 namespace nlohmann {
 	// It's kinda unsafe to allocate pointers through deserialization, since we don't know if the code might not already have raw copies of this poiner somewhere
